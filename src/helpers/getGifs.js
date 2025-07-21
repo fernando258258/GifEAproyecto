3b76680b2a6c62e69443c62f8a297a8c939929c8
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-21 01:36:14 -0400

    className - Clases de css

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -4,7 +4,7 @@ export const getGifs = async (category) => {
   const { data } = await resp.json();
   const gifs = data.map((img) => ({
     id: img.id,
-    tittle: img.tittle,
+    title: img.title,
     url: img.images.downsized_medium.url,
   }));
 
